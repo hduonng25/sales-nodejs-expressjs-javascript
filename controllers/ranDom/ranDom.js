5f61dac3459f53353a2500b43631e36896c8231a
hduonng25 <honngduong25@gmail.com>
2023-11-22 21:37:15 +0700

    hduong

--- controllers/ranDom/ranDom.js
+++ controllers/ranDom/ranDom.js
@@ -1,11 +1,14 @@
 const RandomPassword = function () {
     const length = 8;
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789<>.,.?/}]{]+_-)(*&^%$#@!=";
-    let randomPassword = '';
+    const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789<>.,.?/}]{]+_-)(*&^%$#@!=";
+    let randomPassword = "";
     const charactersLength = characters.length;
 
     for (let i = 0; i < length; i++) {
-        randomPassword += characters.charAt(Math.floor(Math.random() * charactersLength));
+        randomPassword += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
     }
 
     return randomPassword;
