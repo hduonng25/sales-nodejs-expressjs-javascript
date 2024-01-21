cf15030f9816fcb892143e8b0220127f0767a2dc
hduonng25 <honngduong25@gmail.com>
2024-01-21 15:15:11 +0700

    hduong

--- controllers/product/Product.Controller.js
+++ controllers/product/Product.Controller.js
@@ -1,14 +1,14 @@
-import product from "../../models/Product.js";
-import color from "../../models/Color.js";
-import size from "../../models/Size.js";
-import metarial from "../../models/Metarial.js";
-import designs from "../../models/Designs.js";
-import { OK } from "../../constant/HttpResponeCode.js";
-import { v1 } from "uuid";
-import { success } from "../../respone/Respone.Util.js";
+import product from '../../models/Product.js';
+import color from '../../models/Color.js';
+import size from '../../models/Size.js';
+import metarial from '../../models/Metarial.js';
+import designs from '../../models/Designs.js';
+import { OK } from '../../constant/HttpResponeCode.js';
+import { v1 } from 'uuid';
+import { success } from '../../respone/Respone.Util.js';
 
 const create_date = new Date();
-const create_by = "hduong";
+const create_by = 'hduong';
 
 export async function getProducts() {
     const list = await product.find({ is_deleted: false });
@@ -23,7 +23,7 @@ export async function createProduct(
     color_id,
     size_id,
     metarial_id,
-    designs_id
+    designs_id,
 ) {
     const colors = await color.find({ id: color_id });
     const sizes = await size.find({ id: size_id });
@@ -84,7 +84,7 @@ export async function updateProduct(id, price, name, note) {
     const update = await product.findOneAndUpdate(
         { id: id },
         { $set: { price: price, name: name, note: note } },
-        { new: true }
+        { new: true },
     );
 
     return success(update);
@@ -93,7 +93,7 @@ export async function updateProduct(id, price, name, note) {
 export async function deletedProduct(id) {
     const deleted = await product.findOneAndUpdate(
         { id: id },
-        { $set: { is_deleted: true } }
+        { $set: { is_deleted: true } },
     );
     return success(deleted);
 }
@@ -109,23 +109,23 @@ export async function updateProductDetails(
     product_details_id,
     quantity,
     status,
-    is_deleted
+    is_deleted,
 ) {
     const filter = [
         {
             $match: {
                 id: product_id,
-                "product_details.id": product_details_id,
+                'product_details.id': product_details_id,
                 is_deleted: false,
             },
         },
-        { $unwind: "$product_details" },
-        { $match: { "product_details.id": product_details_id } },
+        { $unwind: '$product_details' },
+        { $match: { 'product_details.id': product_details_id } },
         {
             $set: {
-                "product_details.quantity": quantity,
-                "product_details.status": status,
-                "product_details.is_deleted": is_deleted,
+                'product_details.quantity': quantity,
+                'product_details.status': status,
+                'product_details.is_deleted': is_deleted,
             },
         },
     ];
@@ -134,15 +134,23 @@ export async function updateProductDetails(
 
     const update = await product.findOneAndUpdate(
         { id: product_id },
-        { $set: { product_details: product_update[0].product_details } }
+        {
+            $set: {
+                product_details: product_update[0].product_details,
+            },
+        },
     );
     return success(update);
 }
 
-export async function saveImage(product_id, product_details_id, image) {
+export async function saveImage(
+    product_id,
+    product_details_id,
+    image,
+) {
     const products = await product.findOne({ id: product_id });
     const product_details = products.product_details.find(
-        (details) => details.id === product_details_id
+        (details) => details.id === product_details_id,
     );
     product_details.image.push({
         id: v1(),
--- middleware/Sanitization.js
+++ middleware/Sanitization.js
@@ -1,22 +1,22 @@
-import sanitize from "mongo-sanitize";
+import sanitize from 'mongo-sanitize';
 
 export default function validate_api(request, _, next) {
-  const checkSpecialCharacter = (object) => {
-    const clone = JSON.parse(JSON.stringify(object));
-    const cleaned = sanitize(object);
-    return JSON.stringify(clone) !== JSON.stringify(cleaned);
-  };
-  const statuses = [
-    checkSpecialCharacter(request.body),
-    checkSpecialCharacter(request.params),
-    checkSpecialCharacter(request.query),
-  ];
-  if (statuses.some((v) => v === true)) {
-    next({
-      statusCode: 400,
-      error: "DATA_INVALID",
-      description: "Data input is not valid",
-    });
-  }
-  next();
+    const checkSpecialCharacter = (object) => {
+        const clone = JSON.parse(JSON.stringify(object));
+        const cleaned = sanitize(object);
+        return JSON.stringify(clone) !== JSON.stringify(cleaned);
+    };
+    const statuses = [
+        checkSpecialCharacter(request.body),
+        checkSpecialCharacter(request.params),
+        checkSpecialCharacter(request.query),
+    ];
+    if (statuses.some((v) => v === true)) {
+        next({
+            statusCode: 400,
+            error: 'DATA_INVALID',
+            description: 'Data input is not valid',
+        });
+    }
+    next();
 }
