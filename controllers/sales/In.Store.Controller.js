573d1b3a8190dcf7d839ee9c56acf00fb98cedd5
hduonng25 <honngduong25@gmail.com>
2024-02-23 20:09:54 +0700

    hduong

--- controllers/sales/In.Store.Controller.js
+++ controllers/sales/In.Store.Controller.js
@@ -1,15 +1,19 @@
-import product from "../../models/Product.js";
-import bills from "../../models/Bills.js";
-import { v1 } from "uuid";
-import { error, success } from "../../respone/Respone.Util.js";
-import discount from "../../models/Discount.js";
-import user from "../../models/User.js";
+import product from '../../models/Product.js';
+import bills from '../../models/Bills.js';
+import { v1 } from 'uuid';
+import { error, success } from '../../respone/Respone.Util.js';
+import discount from '../../models/Discount.js';
+import user from '../../models/User.js';
 
 const create_date = new Date();
-const create_by = "hduong";
+const create_by = 'hduong';
 
 export async function createBill(id) {
-    const idBig = await bills.findOne({}, { id: 1 }, { sort: { id: -1 } });
+    const idBig = await bills.findOne(
+        {},
+        { id: 1 },
+        { sort: { id: -1 } },
+    );
     const idBill = parseInt(idBig.id) + 1;
     const users = await user.findOne({ id: id });
     await bills.create({
@@ -17,10 +21,10 @@ export async function createBill(id) {
         type_bill: 1,
         is_deleted: false,
         status_bill: 6,
-        code: "HD" + idBill,
+        code: 'HD' + idBill,
         create_by: users.name,
     });
-    return success("create done");
+    return success('create done');
 }
 
 export async function addProductToBill(
@@ -28,23 +32,26 @@ export async function addProductToBill(
     product_id,
     color_id,
     size_id,
-    quantity
+    quantity,
 ) {
     const products = await product.findOne({ id: product_id });
     const product_details = products.product_details.find(
         (details) =>
-            details.size.id === size_id && details.color.id === color_id
+            details.size.id === size_id &&
+            details.color.id === color_id,
     );
     if (product_details) {
         const bill = await bills.findOne({ id: bill_id });
         const money_bill_details = products.price * quantity;
 
         const checkProductOfBill = bill.bill_details.find(
-            (details) => details.product_details_id === product_details.id
+            (details) =>
+                details.product_details_id === product_details.id,
         );
         if (checkProductOfBill) {
             checkProductOfBill.quantity += quantity;
-            checkProductOfBill.money_bill_details += money_bill_details;
+            checkProductOfBill.money_bill_details +=
+                money_bill_details;
         } else {
             bill.bill_details.push({
                 id: v1(),
@@ -64,44 +71,49 @@ export async function addProductToBill(
 
         await products.save();
         await bill.save();
-        return success("add done");
+        return success('add done');
     }
-    return error("product NaN");
+    return error('product NaN');
 }
 
 export async function editQuantityBillDetails(
     bill_id,
     bill_details_id,
-    quantity
+    quantity,
 ) {
-    const bill = await bills.findOne({ "bill_details.id": bill_details_id });
+    const bill = await bills.findOne({
+        'bill_details.id': bill_details_id,
+    });
     const result = await bills.aggregate([
         {
             $match: {
                 id: bill_id,
-                "bill_details.id": bill_details_id,
+                'bill_details.id': bill_details_id,
             },
         },
-        { $unwind: "$bill_details" },
-        { $match: { "bill_details.id": bill_details_id } },
+        { $unwind: '$bill_details' },
+        { $match: { 'bill_details.id': bill_details_id } },
         {
             $set: {
-                "bill_details.quantity": quantity,
-                "bill_details.money_bill_details": {
-                    $multiply: ["$bill_details.price", quantity],
+                'bill_details.quantity': quantity,
+                'bill_details.money_bill_details': {
+                    $multiply: ['$bill_details.price', quantity],
                 },
             },
         },
     ]);
 
     await product.updateOne(
-        { "product_details.id": result[0].bill_details.product_details_id },
+        {
+            'product_details.id':
+                result[0].bill_details.product_details_id,
+        },
         {
             $inc: {
-                "product_details.$.quantity":
+                'product_details.$.quantity':
                     bill.bill_details[0].quantity - quantity,
             },
-        }
+        },
     );
 
     await bills.updateOne({ id: bill_id }, result[0]);
@@ -111,20 +123,20 @@ export async function editQuantityBillDetails(
 
 export async function deletedBillDetails(bill_details_id) {
     await bills.updateOne(
-        { "bill_details.id": bill_details_id },
-        { $set: { "bill_details.$.is_deleted": false } }
+        { 'bill_details.id': bill_details_id },
+        { $set: { 'bill_details.$.is_deleted': false } },
     );
-    return success("delete done");
+    return success('delete done');
 }
 
 export async function payment_in_store(id) {
     await bills.updateOne({ id: id }, { $set: { status_bill: 7 } });
-    return success("payment done");
+    return success('payment done');
 }
 
 export async function pancellation(id) {
     await bills.updateOne({ id: id }, { $set: { status_bill: 8 } });
-    return success("pancellation done");
+    return success('pancellation done');
 }
 
 export async function addDiscount(discount_id, order_id) {
@@ -144,7 +156,7 @@ export async function addDiscount(discount_id, order_id) {
                     bill_money: money_update,
                     discount_id: discounts.id,
                 },
-            }
+            },
         );
     } else {
         money = reduced_money;
@@ -157,8 +169,8 @@ export async function addDiscount(discount_id, order_id) {
                     bill_money: money_update,
                     discount_id: discounts.id,
                 },
-            }
+            },
         );
     }
-    return success("done");
+    return success('done');
 }
--- routers/bill/online/Pending.Confirmation.js
+++ routers/bill/online/Pending.Confirmation.js
@@ -1,105 +1,146 @@
-import express from "express";
+import express from 'express';
 import {
-  find,
-  findDateBill,
-  getBill,
-  getDetails,
-  updateStatus,
-} from "../../../controllers/bill/Online.Controller.js";
-import { format, parseISO } from "date-fns";
+    find,
+    findDateBill,
+    getBill,
+    getDetails,
+    updateStatus,
+} from '../../../controllers/bill/Online.Controller.js';
+import { format, parseISO } from 'date-fns';
 import {
-  check_id_query,
-  check_id_body,
-  check_search,
-} from "../../../validator/Base.js";
+    check_id_query,
+    check_id_body,
+    check_search,
+} from '../../../validator/Base.js';
 
 const router = express.Router();
 
-router.get("/list", async (request, respone, next) => {
-  const list = await getBill(1);
-  next(list);
-});
-
-router.put("/confirm", check_id_body, async (request, respone, next) => {
-  const { id } = request.body;
-  const id_user = request.payload.id;
-  await updateStatus(id, id_user, 2, "Chuyển trạng thái sang chờ giao hàng");
-  const list = await getBill(1);
-  next(list);
-});
-
-router.put("/cancel", check_id_body, async (request, respone, next) => {
-  const { id } = request.body;
-  await updateStatus(id, 5, "Chuyển trạng thu đơn hàng sang đã hủy");
-  const list = await getBill(1);
-  next(list);
-});
-
-router.put("/confirm-all", async (request, respone, next) => {
-  const list = (await getBill(1)).data;
-
-  for (const bill of list) {
-    const id = bill.id;
-    await updateStatus(id, 2, "Chuyển trạng thái sang chờ giao hàng");
-  }
-
-  const listReturn = await getBill(1);
-  next(listReturn);
-});
-
-router.put("/cancel-all", async (request, respone, next) => {
-  const list = (await getBill(1)).data;
-  for (const bill of list) {
-    const id = bill.id;
-    await updateStatus(id, 5, "Chuyển trạng thái sang chờ đã huy");
-  }
-  const listReturn = await getBill(1);
-  next(listReturn);
+router.get('/list', async (request, respone, next) => {
+    const list = await getBill(1);
+    next(list);
 });
 
 router.put(
-  "/confirm-selected",
-  check_id_body,
-  async (request, respone, next) => {
-    const idSelected = request.body.id;
-    for (const id of idSelected) {
-      await updateStatus(id, 2, "Chuyển trạng thái sang chờ giao hàng");
-    }
-    const listReturn = await getBill(1);
-    next(listReturn);
-  }
+    '/confirm',
+    check_id_body,
+    async (request, respone, next) => {
+        const { id } = request.body;
+        const id_user = request.payload.id;
+        await updateStatus(
+            id,
+            id_user,
+            2,
+            'Chuyển trạng thái sang chờ giao hàng',
+        );
+        const list = await getBill(1);
+        next(list);
+    },
 );
 
 router.put(
-  "/cannel-selected",
-  check_id_body,
-  async (request, respone, next) => {
-    const idSelected = request.body.id;
-    for (const id of idSelected) {
-      await updateStatus(id, 5, "Chuyển trạng thái sang đã hủy");
+    '/cancel',
+    check_id_body,
+    async (request, respone, next) => {
+        const { id } = request.body;
+        await updateStatus(
+            id,
+            5,
+            'Chuyển trạng thu đơn hàng sang đã hủy',
+        );
+        const list = await getBill(1);
+        next(list);
+    },
+);
+
+router.put('/confirm-all', async (request, respone, next) => {
+    const list = (await getBill(1)).data;
+
+    for (const bill of list) {
+        const id = bill.id;
+        await updateStatus(
+            id,
+            2,
+            'Chuyển trạng thái sang chờ giao hàng',
+        );
+    }
+
+    const listReturn = await getBill(1);
+    next(listReturn);
+});
+
+router.put('/cancel-all', async (request, respone, next) => {
+    const list = (await getBill(1)).data;
+    for (const bill of list) {
+        const id = bill.id;
+        await updateStatus(
+            id,
+            5,
+            'Chuyển trạng thái sang chờ đã huy',
+        );
     }
     const listReturn = await getBill(1);
     next(listReturn);
-  }
+});
+
+router.put(
+    '/confirm-selected',
+    check_id_body,
+    async (request, respone, next) => {
+        const idSelected = request.body.id;
+        for (const id of idSelected) {
+            await updateStatus(
+                id,
+                2,
+                'Chuyển trạng thái sang chờ giao hàng',
+            );
+        }
+        const listReturn = await getBill(1);
+        next(listReturn);
+    },
 );
 
-router.get("/find", check_search, async (request, respone, next) => {
-  const search = request.query.search;
-  const listFind = await find(1, search);
-  next(listFind);
+router.put(
+    '/cannel-selected',
+    check_id_body,
+    async (request, respone, next) => {
+        const idSelected = request.body.id;
+        for (const id of idSelected) {
+            await updateStatus(
+                id,
+                5,
+                'Chuyển trạng thái sang đã hủy',
+            );
+        }
+        const listReturn = await getBill(1);
+        next(listReturn);
+    },
+);
+
+router.get('/find', check_search, async (request, respone, next) => {
+    const search = request.query.search;
+    const listFind = await find(1, search);
+    next(listFind);
 });
 
-router.get("/find/date", check_search, async (request, respone, next) => {
-  const date = request.query.search;
-  const search = format(parseISO(date), "yyyy-MM-dd");
-  const listFind = await findDateBill(1, search);
-  next(listFind);
-});
+router.get(
+    '/find/date',
+    check_search,
+    async (request, respone, next) => {
+        const date = request.query.search;
+        const search = format(parseISO(date), 'yyyy-MM-dd');
+        const listFind = await findDateBill(1, search);
+        next(listFind);
+    },
+);
 
-router.get("/details", check_id_query, async (request, respone, next) => {
-  const { id } = request.query;
-  const details = await getDetails(id, 1);
-  next(details);
-});
+router.get(
+    '/details',
+    check_id_query,
+    async (request, respone, next) => {
+        const { id } = request.query;
+        const details = await getDetails(id, 1);
+        next(details);
+    },
+);
 
 export default router;
